6f3a7bddec8b9ac693fef3d93a61a4cf483eae73
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2022-12-01 17:07:51 +0100

    Create AdventOfCode2022 + Day 1

---
+++ Days/Day1.cs
@@ -0,0 +1,28 @@
+﻿namespace AdventOfCode2022.Days;
+
+public class Day1
+{
+    public static void Execute()
+    {
+        var file = "Resources/1-input.txt";
+        var gobelins = new List<int>();
+
+        foreach (var line in File.ReadAllLines(file))
+        {
+            if(line == "" || gobelins.Count == 0)
+                gobelins.Add(0);
+            else
+                gobelins[^1] += Convert.ToInt32(line);
+        }
+        
+        Console.WriteLine($"First Anwser : {gobelins.Max()}");
+
+        var total = gobelins.Max();
+        gobelins.Remove(gobelins.Max());
+        total += gobelins.Max();
+        gobelins.Remove(gobelins.Max());
+        total += gobelins.Max();
+        
+        Console.WriteLine($"Second Anwser : {total}");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using AdventOfCode2022.Days;
+
+namespace AdventOfCode2022;
+
+internal class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter day : ");
+        try
+        {
+            var day = Convert.ToInt32(Console.ReadLine());
+            switch (day)
+            {
+                case 1:
+                    Day1.Execute();
+                    return;
+                default:
+                    Console.WriteLine("This day doesn't exist");
+                    return;
+            }
+        }
+        catch (Exception ex)            
+        {
+            if (ex is not (FormatException or OverflowException)) throw;
+                
+            Console.WriteLine("Give a valid number for day.");
+            return;
+
+        }
+    }
+}
+
