53b6717246d929ced52d53e6d4f8b718a9724a79
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2022-12-11 20:34:58 +0100

    Day 11 (Doesn't work)

---
+++ Days/Day11.cs
@@ -0,0 +1,79 @@
+﻿namespace AdventOfCode2022.Days;
+
+public class Day11: Day
+{
+    class Monkey
+    {
+        public int Nb;
+        public List<int> Items;
+        public string Operation;
+        public int Test;
+        public int TrueMonkey;
+        public int FalseMonkey;
+        public int Inspect = 0;
+
+        public Monkey(int nb, List<int> items, string operation, int test, int trueMonkey, int falseMonkey)
+        {
+            Nb = nb;
+            Items = items;
+            Operation = operation;
+            Test = test;
+            TrueMonkey = trueMonkey;
+            FalseMonkey = falseMonkey;
+        }
+        
+        public int ApplyOperation(int nb)
+        {
+            if (Operation[4] == '+')
+            {
+                if (Operation.Split(" + ")[1] == "old")
+                    return (int)Math.Floor(nb + nb / 3d);
+                return (int)Math.Floor((nb + Convert.ToInt32(Operation.Split(" + ")[1])) / 3d);
+            }
+            if (Operation.Split(" * ")[1] == "old")
+                return (int)Math.Floor(nb * nb / 3d);
+            return (int)Math.Floor(nb * Convert.ToInt32(Operation.Split(" * ")[1]) / 3d);
+        }
+
+        public static Monkey FromString(IReadOnlyList<string> monkeyLines)
+        {
+            var nb = Convert.ToInt32(monkeyLines[0][^2].ToString());
+            var list = monkeyLines[1].Split(": ")[1].Split(", ").Select(x => Convert.ToInt32(x)).ToList();
+            var operation = monkeyLines[2].Split(" = ")[1];
+            var test = Convert.ToInt32(monkeyLines[3].Split("by ")[1]);
+            var trueMonkey = Convert.ToInt32(monkeyLines[4][^1].ToString());
+            var falseMonkey = Convert.ToInt32(monkeyLines[5][^1].ToString());
+            return new Monkey(nb, list, operation, test, trueMonkey, falseMonkey);
+        }
+    }
+
+
+    public void Execute()
+    {
+        var monkeysLines = File.ReadAllText("Resources/11-input.txt").Replace("\r", "").Split("\n\n");
+        var monkeys = monkeysLines.Select(monkeyLine => Monkey.FromString(monkeyLine.Split("\n"))).ToList();
+        
+        for (var i = 0; i < 20; i++)
+        {
+            foreach (var monkey in monkeys)
+            {
+                var nb = monkey.Items.Count;
+                while (nb > 0)
+                {
+                    monkey.Inspect++;
+                    var item = monkey.Items[0];
+                    item = monkey.ApplyOperation(item);
+                    if(item % monkey.Test == 0)
+                        monkeys[monkey.TrueMonkey].Items.Add(item);
+                    else
+                        monkeys[monkey.FalseMonkey].Items.Add(item);
+                    monkey.Items.RemoveAt(0);
+                    nb--;
+                }
+            }
+        }
+        
+        monkeys.Sort((m1, m2) => m1.Inspect.CompareTo(m2.Inspect));
+        Console.WriteLine($"First Answer : {monkeys[^1].Inspect * monkeys[^2].Inspect} ({monkeys[^1].Inspect} * {monkeys[^2].Inspect})");
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,10 +6,10 @@ internal class Program
 {
     static readonly List<Day> Days = new()
     {
-        new Day1(), new Day2(), new Day3(),
-        new Day4(), new Day5(), new Day6(), 
-        new Day7(), new Day8(), new Day9(),
-        new Day10()
+        new Day1(),  new Day2(),  new Day3(),
+        new Day4(),  new Day5(),  new Day6(), 
+        new Day7(),  new Day8(),  new Day9(),
+        new Day10(), new Day11()
     };
 
     static void Main()
@@ -17,23 +17,14 @@ internal class Program
         while (true)
         {
             Console.Write("Enter day (or 0 to quit): ");
-            try
-            {
-                var day = Convert.ToInt32(Console.ReadLine());
-                if (day == 0)
-                    return;
-                
-                if (day > Days.Count)
-                    Console.WriteLine("This day doesn't exist");
-                else
-                    Days[day - 1].Execute();
-            }
-            catch (Exception ex)
-            {
-                if (ex is not (FormatException or OverflowException)) throw;
-
-                Console.WriteLine("Give a valid number for day.");
-            }
+            var day = Convert.ToInt32(Console.ReadLine());
+            if (day == 0)
+                return;
+            
+            if (day > Days.Count)
+                Console.WriteLine("This day doesn't exist");
+            else
+                Days[day - 1].Execute();
         }
     }
 }
